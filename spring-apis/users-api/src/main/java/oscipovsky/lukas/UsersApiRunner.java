3035e4f45a8ade064f0bba96008c07525b177ea7
LukasOscipovsky <lukas.oscipovsky@gmail.com>
2018-05-25 09:27:32 +0200

    user-api basic maven module

---
+++ spring-apis/users-api/src/main/java/oscipovsky/lukas/UsersApiRunner.java
@@ -0,0 +1,12 @@
+package oscipovsky.lukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiRunner.class, args);
+	}
+}
---
+++ spring-apis/users-api/src/main/java/oscipovsky/lukas/controller/HealtController.java
@@ -0,0 +1,14 @@
+package oscipovsky.lukas.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealtController {
+
+	@GetMapping(path = "/health/status")
+	public HttpStatus healthstatus() {
+		return HttpStatus.OK;
+	}
+}
